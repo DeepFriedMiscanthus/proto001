0c3489588e63c7eb224373149dd38c952aae721b
DeepFriedMiscanthus <minhuig2394@gmail.com>
2016-07-30 14:26:07 -0400

    using bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,28 @@
 var mousePosition = {x: -1, y: -1};
 
+function getBoardWidth() {
+	return $(".board").width();
+}
+
+function getBasePixels() {
+	return parseFloat($("body").css("font-size"));
+}
+
+function pixelsToRem(pixels) {
+	return pixels/getBasePixels();
+}
+
+function remToPixels(rem) {
+	return rem*getBasePixels();
+}
+
 function addStickyNote(boardElem) {
 	var newNote = $("<div/>").addClass('sticky');
 
 	var noteTitle = $("<textarea/>").prop("placeholder", "Title").addClass('sticky-title');
 	var noteContent = $("<textarea/>").prop("placeholder", "Write Something").addClass('sticky-content')
-	var deleteButton = $("<a/>").text("x").addClass("sticky-delete-button");
+	var deleteButton = $('<a/>').addClass("sticky-delete-button");
+	$('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>').appendTo(deleteButton);
 	deleteButton.click(function(e) {
 		newNote.fadeOut('200', function() {
 			newNote.remove(); 
@@ -19,10 +36,21 @@ function addStickyNote(boardElem) {
 }
 
 
+function resizeBoardElements() {
+	var numPanels = $(".board-elem").length;
+	var offset = remToPixels(0.2);
+	// var offset = 0;
+	var boardElementWidthInPixels = (getBoardWidth() - offset)/numPanels;
+	// console.log(offset, getBoardWidth(), boardElementWidthInPixels);
+	$(".board-elem")
+	.css({
+		'width': boardElementWidthInPixels
+ 	});
+}
+
 function init() {
 	//initiate styles
-	var numPanels = $(".board-elem").length;
-	$(".board-elem").css('width', "" + 99/numPanels + "%");
+	resizeBoardElements();
 
 	//mouse tracking init : http://stackoverflow.com/questions/4517198/how-to-get-mouse-position-in-jquery-without-mouse-events
 	$( document ).mousemove(function(event) {
@@ -38,8 +66,11 @@ function init() {
 	});
 }   
 
-
+$( window ).resize( function() {
+	resizeBoardElements(); 
+});
 
 $( document ).ready(function () {
 	init();
 });
+
